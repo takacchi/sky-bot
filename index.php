4942f33d9f5fb929b66da8ea8a33588d5322b8b3
takacchi <hisashi@hisashinoMacBook-Pro.local>
2017-09-07 22:55:02 +0900

    update

--- index.php
+++ index.php
@@ -11,15 +11,42 @@ $events = $bot->parseEventRequest($inputString, $signature);
 error_log($inputString);
 
 foreach($events as $event) {
-  replyLocationMessage($bot, $event->getReplyToken(), 'LINE', '東京都渋谷区渋谷2-21-1 ヒカリエ27階',
-                          35.659025, 139.703473);
+  replyButtonsTemplate($bot, $event->getReplyToken(), 'お天気お知らせ - 今日の天気予報は晴れです',
+                  'https://' . $_SERVER['HTTP_HOST'] . '/imgs/template.jpg',
+                  'お天気お知らせ', '今日の天気予報は晴れです。',
+                  new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder('明日の天気', 'tomorrow'),
+                  new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder('週末の天気', 'weekend'),
+                  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Webで見る', 'http://google.jp'));
+
 }
 
 function replyLocationMessage($bot, $replyToken, $title, $address, $lat, $lon) {
   $response = $bot->replyMessage($replyToken, 
         new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder($title, $address, $lat, $lon));
   if (!$response->isSucceeded()) {
+    errorLog($response);
+  }
+}
+
+function replyButtonsTemplate($bot, $replyToken, $alternativeText, $imageUrl, $title, $text, ...$actions) {
+  $actionArray = array();
+  foreach($actions as $value) {
+    array_push($actionArray, $value);
+  }
+
+  $builder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder($alternativeText,
+                   new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder(
+                      $title, $text, $imageUrl, $actionArray));
+  $response = $bot->replyMessage($replyToken, $builder);
+  if (!$response->isSucceeded()) {
+    errorLog($response);
+  }
+}
+
+funtion errorLog($response) {
+  if (!$response->isSucceeded()) {
     error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
   }
 }
+
 ?>
