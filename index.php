5d02a379d619e371bea24df53208c01f9462c91a
takacchi <hisashi@hisashinoMacBook-Pro.local>
2017-09-10 08:50:07 +0900

    update

--- index.php
+++ index.php
@@ -105,8 +105,8 @@ foreach($events as $event) {
 	 if (!isset($max)) { $maxCelsius = "--"; }
 	 $msg = new TextMessageBuilder($json['location']['city'] . 'の天気' . PHP_EOL . $fc['dataLabel'] . PHP_EOL . $fc['telop'] . PHP_EOL . $minCelsius . '/' . $maxCelsius);
 	 $image = new ImageMessageBuilder($image_url, $image_url);
-	 builder->add($msg);
-	 builder->add($image);
+	 $builder->add($msg);
+	 $builder->add($image);
 //	 replyMultiMessage($bot, $event->getReplyToken(), 
 //           new TextMessageBuilder($json['location']['city'] . 'の天気' . PHP_EOL . $fc['dataLabel'] . PHP_EOL . $fc['telop'] . PHP_EOL . $minCelsius . '/' . $maxCelsius));
 //		   new ImageMessageBuilder($image_url, $image_url));
